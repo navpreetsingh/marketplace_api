369dae828c948bf44560f59e3314cbc176e5ac7c
Navpreet Singh <navpreet.singh.k@gmail.com>
2015-06-03 15:51:06 +0530

    chapter-7

---
+++ lib/check.rb
@@ -0,0 +1,7 @@
+class Check
+	def initialize(params)
+		@x = params[0]
+		@y = params[1]
+		@z = params[2]
+	end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,4 +17,78 @@ describe Product do
   it { should validate_presence_of :user_id}
 
   it { should belong_to :user }
+
+  describe ".filter_by_title" do
+    before(:each) do 
+      @product1 = FactoryGirl.create :product, title: "A plasma TV"
+      @product2 = FactoryGirl.create :product, title: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, title: "CD player"
+      @product4 = FactoryGirl.create :product, title: "LCD TV"
+    end
+
+    context "when a 'TV' title pattern is sent" do
+      it "returns the 2 products matching" do
+        expect(Product.filter_by_title("TV")).to have(2).items
+      end
+
+      it "returns the products matching" do
+        expect(Product.filter_by_title("TV").sort).to match_array([@product1, @product4])
+      end
+    end
+  end
+
+  describe ".price_sorting" do
+    before(:each) do 
+      @product1 = FactoryGirl.create :product, price: 100 
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+
+      # touching some products to update them
+      @product2.touch
+      @product3.touch
+    end
+
+    it 'returns the products which are above or equal to the price' do 
+      expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3]) 
+    end
+
+    it "returns the products which are below or equal to the price" do
+      expect(Product.below_or_equal_to_price(99).sort).to match_array([@product2, @product4])
+    end    
+
+    it "returns the most updated records" do
+      expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+    end
+  end
+
+  describe ".search" do 
+    before(:each) do 
+      @product1 = FactoryGirl.create :product, price: 100, title: "Plasma TV"
+      @product2 = FactoryGirl.create :product, price: 50, title: "Videogame Console"
+      @product3 = FactoryGirl.create :product, price: 150, title: "MP3"
+      @product4 = FactoryGirl.create :product, price: 99, title: "Laptop"
+    end
+
+    context "when title 'videogame' and '100' a min price are set" do
+      it "returns an empty array" do
+        search_hash = { keyword: "videogame", min_price: 100 }
+        expect(Product.search(search_hash)).to be_empty
+      end
+    end
+
+    context "when an empty hash is sent" do
+      it "returns all the products" do
+        expect(Product.search({})).to match_array([@product1, @product2, @product3, @product4])
+      end
+    end
+
+    context "when product_ids is present" do
+      it "returns the product from the ids" do
+        search_hash = { product_ids: [@product1.id, @product2.id]}
+        expect(Product.search(search_hash)).to match_array([@product1, @product2])
+      end
+    end
+  end
+
 end
